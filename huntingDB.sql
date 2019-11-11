df180e6ef8c429ef2f4a6988daf37327b168f083
sam <samluther998@gmail.com>
2019-11-11 15:44:47 -0700

    Some basic sample data

--- huntingDB.sql
+++ huntingDB.sql
@@ -73,26 +73,35 @@ INSERT INTO Hunter (Hunter_id, Fname,Minit, Lname, Resident)
 VALUES  (111111, "Jeff", "L", "Baxter", "Montana"),
         (111112, "Bill", "T", "Boible", "Montana"),
         (111113, "Freddy", "G", "Mercury", "North Dakota"),
-        (123456, "Koch", "E", "Babcock", "Idaho"),
+        (123456, "Koch", "E", "Babcock", "Idaho"), 
         (123457, "Gooch", "F", "Hill", "Montana"),
         (109344, "Alfred", "D", "Stucky", "Montana"),
         (108457, "John", "Q", "Wonton", "Montana"),
         (123467, "Beese", "M", "Churger", "Wyoming"); 
 
 INSERT INTO Tags (Tag_id, Hunter_id,District_id, Animal, Bow_rifle, Liscense_year)
-VALUES  (111111, "Jeff", "L", "Baxter", "Montana"),
-        (111112, "Bill", "T", "Boible", "Montana"),
-        (111113, "Freddy", "G", "Mercury", "North Dakota"),
-        (123456, "Koch", "E", "Babcock", "Idaho"),
-        (123457, "Gooch", "F", "Hill", "Montana"),
-        (109344, "Alfred", "D", "Stucky", "Montana"),
-        (108457, "John", "Q", "Wonton", "Montana"),
-        (123467, "Beese", "M", "Churger", "Wyoming"); 
+VALUES  (0001, 111111, 1, "Elk Bull", "Rifle", 2019),
+        (0002, 111111, 2, "Mule Deer Buck", "Rifle", 2018),
+        (0003, 111113, 3, "Elk Cow", "Bow", 2019),
+        (0004, 123467, 4, "Elk Bull", "Rifle", 2017),
+        (0005, 108457, 1, "Elk Cow", "Bow", 2019),
+        (0006, 108457, 2, "Bear", "Rifle", 2019),
+        (0007, 123457, 3, "White Tail Deeer Doe", "Rifle", 2018),
+        (0008, 123457, 7, "Elk Cow", "Bow", 2020); 
 
+INSERT INTO Employee (Ssn, Fname, Minit, Lname, Hours_worked)
+VALUES  (000112222, "Saul", "T", "Bowser", "40"),
+        (111223333, "Jackson", "A", "Woofer", "20"),
+        (222334444, "Thomas", "D", "Woofer", "32"),
+        (123456789, "Caleb", "West", "Couser", null);H
 
--- Tag_id integer,
--- Hunter_id integer references Hunter(Hunter_id), 
--- District_id integer references District(District_id),
--- Animal text,
--- Bow_rifle text,
--- Liscense_year integer,
+INSERT INTO Hunting_trip(Trip_id, Tag_id, Hunter_id, ESsn, Points, First_year)
+VALUES  (001, 0002, 111111, 000112222, "Mule Deer Buck", 16, "false"),
+        (002, 0004, 123467, 111223333, "Elk Bull", 4, "true"),
+        (003, 0007, 123457, 222334444, "Mule Deer Doe", null, "false"),
+
+INSERT INTO Harvest_estimate(Liscence_year, Disctrict, Num_hunters, Residency, Total_harvest, Days_hunted, Num_males, Num_females, Num_first_years, Num_points)
+VALUES  (2017, 1, 15, "Montana", 3, 15, 30, 25, 10, 50),
+        (2018, 1, 15, "Montana", 3, 15, 15, 20, 6, 35),
+        (2019, 1, 15, "Montana", 3, 15, 22, 30, 18, 45);
+
