352f8d6f7c61e02683a2cac2486332463eed7792
sam <samluther998@gmail.com>
2019-11-11 15:23:25 -0700

    first round of samples.. more coming soon

--- huntingDB.sql
+++ huntingDB.sql
@@ -59,3 +59,40 @@ Num_points integer);
 
 
 /* Populate the tables with our data */
+
+INSERT INTO District (District_id, Area_coordinates)
+VALUES  (1, "(0,0),(3,0),(0,3),(3,3)"),
+        (2, "(3,3),(6,3),(3,6),(6,6)"),
+        (3, "(0,3),(3,3),(0,6),(3,6)"),
+        (4, "(3,0),(6,0),(3,3),(6,3)"),
+        (5, "(6,6),(9,6),(6,9),(9,9)"),
+        (6, "(0,6),(3,6),(0,9),(3,9)"),
+        (7, "(6,0),(9,0),(6,3),(9,3)"); 
+
+INSERT INTO Hunter (Hunter_id, Fname,Minit, Lname, Resident)
+VALUES  (111111, "Jeff", "L", "Baxter", "Montana"),
+        (111112, "Bill", "T", "Boible", "Montana"),
+        (111113, "Freddy", "G", "Mercury", "North Dakota"),
+        (123456, "Koch", "E", "Babcock", "Idaho"),
+        (123457, "Gooch", "F", "Hill", "Montana"),
+        (109344, "Alfred", "D", "Stucky", "Montana"),
+        (108457, "John", "Q", "Wonton", "Montana"),
+        (123467, "Beese", "M", "Churger", "Wyoming"); 
+
+INSERT INTO Tags (Tag_id, Hunter_id,District_id, Animal, Bow_rifle, Liscense_year)
+VALUES  (111111, "Jeff", "L", "Baxter", "Montana"),
+        (111112, "Bill", "T", "Boible", "Montana"),
+        (111113, "Freddy", "G", "Mercury", "North Dakota"),
+        (123456, "Koch", "E", "Babcock", "Idaho"),
+        (123457, "Gooch", "F", "Hill", "Montana"),
+        (109344, "Alfred", "D", "Stucky", "Montana"),
+        (108457, "John", "Q", "Wonton", "Montana"),
+        (123467, "Beese", "M", "Churger", "Wyoming"); 
+
+
+-- Tag_id integer,
+-- Hunter_id integer references Hunter(Hunter_id), 
+-- District_id integer references District(District_id),
+-- Animal text,
+-- Bow_rifle text,
+-- Liscense_year integer,
